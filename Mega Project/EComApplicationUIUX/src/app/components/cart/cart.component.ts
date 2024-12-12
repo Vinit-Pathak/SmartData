24678df2880d64429f9a959b6b7cd981ccb4ecfc
Vinit Pathak <pathakvinit32@gmail.com>
2024-12-12 18:41:33 +0530

    EHR Added

--- Mega Project/EComApplicationUIUX/src/app/components/cart/cart.component.ts
+++ Mega Project/EComApplicationUIUX/src/app/components/cart/cart.component.ts
@@ -10,6 +10,8 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { CountryStateService } from '../../service/countryState/country-state.service';
+import Swal from 'sweetalert2';
 declare var bootstrap: any;
 
 @Component({
@@ -23,6 +25,7 @@ export class CartComponent {
   cartService = inject(CartService);
   toasterService = inject(ToastrService);
   userService = inject(UserService);
+  countryStateService = inject(CountryStateService);
   router = inject(Router)
   cartItems: any[] = [];
   userId: number = 0;
@@ -46,18 +49,107 @@ export class CartComponent {
     });
 
     this.cartService.updateCartItemCount();
-
+    this.getAllCountry()
     var data = JSON.parse(localStorage.getItem('userData') || '{}');
     this.imgURL = data.profileImage;
+    this.sanitizeField('expiryDate')
   }
 
   paymentForm = new FormGroup({
     cardNumber: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{16}$')]),
     expiryDate: new FormControl('', [
-      Validators.required
+      Validators.required,
+      Validators.pattern(/^[0-1][0-9]\/\d{2}$/),
     ]),
     cvv: new FormControl('',[Validators.required, Validators.pattern('^[0-9]{3}$')]),
   });
+
+  sanitizeField(field: string) {
+    this.paymentForm.get(field)?.valueChanges.subscribe((value) => {
+      const sanitizedValue = value.replace(/[^0-9/]/g, '');
+      const limitedValue = sanitizedValue.slice(0, 5);
+
+      if (limitedValue !== value) {
+        this.paymentForm.get(field)?.setValue(limitedValue);
+      }
+    });
+  }
+
+  addressForm: FormGroup = new FormGroup({
+    address: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(150),
+      Validators.pattern(/^[a-zA-Z0-9\s,.-]+$/),
+    ]),
+    zipCode: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\d{6}$/),
+      Validators.minLength(6),
+      Validators.maxLength(6),
+    ]),
+    state: new FormControl(0, [Validators.required]),
+    country: new FormControl(0, [Validators.required]),
+  });
+
+  openAddressPopup() {
+    if (this.users.address) {
+      this.addressForm.patchValue({
+        address: this.users.address,
+        zipCode: this.users.zipCode,
+        country: this.users.country,
+        state: this.users.state,
+      });
+    }
+
+    this.getState(this.users.country);
+    // Show the modal by adding the class
+    const modal = document.getElementById('addressModal');
+    if (modal) {
+      modal.classList.add('show');
+      modal.style.display = 'block';
+    }
+  }
+
+  addressData: any;
+  saveAddress() {
+    this.addressData = this.addressForm.value;
+    this.closeAddressModal('addressModal');
+    this.openPaymentModal();
+  }
+
+  closeAddressModal(modalId: string) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+      modal.classList.remove('show');
+      modal.style.display = 'none';
+    }
+  }
+
+  allCountry: any[] = [];
+
+  getAllCountry() {
+    this.countryStateService.getAllCountry().subscribe({
+      next: (res: any) => {
+        this.allCountry = res;
+      },
+      error: (error: any) => {
+        alert('I am in error');
+      },
+    });
+  }
+
+  allState: any[] = [];
+  getState(countrId: any) {
+    this.countryStateService.getStateByCountryId(countrId).subscribe({
+      next: (res: any) => {
+        this.allState = res;
+      },
+      error: (error: any) => {
+        alert('I am in error');
+      },
+    });
+  }
 
   onKeyPress(event: KeyboardEvent) {
     const charCode = event.which ? event.which : event.keyCode;
@@ -92,10 +184,10 @@ export class CartComponent {
         expiryDate: formattdExpiryDate,
         cvv: this.paymentForm.get('cvv')?.value,
         userId: this.users.id,
-        zipCode: this.users.zipCode,
-        address: this.users.address,
-        state: this.users.state,
-        country: this.users.country,
+        zipCode: this.addressData.zipCode,
+        address: this.addressData.address,
+        state: this.addressData.state,
+        country: this.addressData.country,
       }
 
       console.log('Payload for validating card:', payload);
@@ -263,12 +355,30 @@ export class CartComponent {
     }
   }
 
+
+  confirmDeletion(productId: any): void {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You will not be able to recover this item!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, cancel!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Perform the delete operation
+        this.removeCartItem(productId);
+        
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        // Swal.fire('Cancelled', 'Your item is safe.', 'info');
+      }
+    });
+  }
+
   removeCartItem(cartId: any): void {
-    console.log('cartId', cartId);
-    const isConfirm = confirm(
-      'Are you sure you want to remove this item from cart?'
-    );
-    if (isConfirm) {
+  
       this.cartService.removeFromCart(cartId).subscribe({
         next: (res: any) => {
           if (res.statusCode === 200) {
@@ -284,9 +394,6 @@ export class CartComponent {
           this.toasterService.error('Error removing item from cart');
         },
       });
-    } else {
-      console.log('Cancelled');
-    }
   }
   
   }
